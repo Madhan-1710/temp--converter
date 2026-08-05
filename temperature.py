898e99c8cacc9de69cb21056989af8a568fb8b03
Madhan-1710 <madhankumaran1710@gmail.com>
2026-08-05 16:58:57 +0530

    Add  function fahrenheit_to_celsius(f)

--- temperature.py
+++ temperature.py
@@ -3,6 +3,10 @@
 def  celsius_to_fahrenheit(celsius):
       ans = (celsius * 9/5) + 32
       print(ans)
+def  celsius_to_kelvin(c):
+        ans = c + 273.15
+        print(ans)
 
 celcius =25
 celsius_to_fahrenheit(celcius)
+celsius_to_kelvin(celcius)
